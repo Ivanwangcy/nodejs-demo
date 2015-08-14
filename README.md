5ee9ba87c2804cbfd9cf535b86e1c933270751e9
Ivan <wcy5211678@163.com>
2015-08-14 12:56:51 +0800

    加载不了mongoDB comment code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 #nodeJS服务器端开发示例
-采用ejs模块引擎, Express框架
+采用ejs模块引擎, Express框架<br>
+运行项目需执行:
+```shell
+npm install
+npm start
+```
--- routes/movie.js
+++ routes/movie.js
@@ -4,8 +4,8 @@
 var express = require('express');
 var movie = express.Router();
 
-var Movie = require('./../models/Movie.js');
-console.log("Movie" + Movie);
+// var Movie = require('./../models/Movie.js');
+// console.log("Movie" + Movie);
 movie.get('/add', function(req, res, next) {
     if(req.params.name){//update
         return res.render('movie', {
